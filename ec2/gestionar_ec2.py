9e5d03a44f4c5cb58d3a9ba364bda3e96bcd2ce1
Karol Torres <steffycuatri93@gmail.com>
2026-04-06 01:39:07 +0000

    feat: iniciar instancias EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -4,8 +4,22 @@ import sys
 ec2 = boto3.client('ec2')
 
 def ejecutar(accion, instance_id=None):
-   if accion == "listar":
-       data = ec2.describe_instances()
-       for r in data["Reservations"]:
-           for i in r["Instances"]:
-               print(i["InstanceId"], i["State"]["Name"])
+
+    if accion == "listar":
+        try:
+            data = ec2.describe_instances()
+            for r in data["Reservations"]:
+                for i in r["Instances"]:
+                    print(i["InstanceId"], i["State"]["Name"])
+        except Exception as e:
+            print(f"Error al listar instancias: {e}")
+
+    elif accion == "iniciar":
+        if not instance_id:
+            print("Falta instance_id")
+            return
+        try:
+            ec2.start_instances(InstanceIds=[instance_id])
+            print("Instancia iniciada")
+        except Exception as e:
+            print(f"Error al iniciar instancia: {e}")
