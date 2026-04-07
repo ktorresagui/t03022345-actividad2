10cc02db050001fad5cc919764f11221afafa206
Karol Torres <steffycuatri93@gmail.com>
2026-04-07 02:30:36 +0000

    feat: validacion de directorio

--- s3/backup_s3.sh
+++ s3/backup_s3.sh
@@ -5,3 +5,17 @@ BUCKET=$2
 NOW=$(date +"%Y-%m-%d_%H-%M-%S")
 FILE="respaldo_$NOW.tar.gz"
 LOGFILE="logs/s3.log"
+
+mkdir -p logs
+
+if [ -z "$DIR" ] || [ -z "$BUCKET" ]; then
+  echo "Parámetros incorrectos"
+  echo "Uso: ./script.sh <directorio> <bucket>" >> $LOGFILE
+  exit 1
+fi
+
+if [ ! -d "$DIR" ]; then
+  echo "El directorio no existe"
+  echo "[$NOW] Error: directorio no existe: $DIR" >> $LOGFILE
+  exit 1
+fi
