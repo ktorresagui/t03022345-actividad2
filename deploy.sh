017a1595fd221650deac421e51b99da1e908b38b
Karol Torres <steffycuatri93@gmail.com>
2026-04-07 03:49:30 +0000

    feat: integración con EC2

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,10 @@ accion=$1
 instancia=$2
 carpeta=$3
 bucket=$4
+
+if [ -z "$accion" ] || [ -z "$instancia" ]; then
+    echo "Uso: ./deploy.sh accion instance_id"
+    exit 1
+fi
+
+python3 ec2/gestionar_ec2.py $accion $instancia
